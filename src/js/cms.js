f6e12f5c88823e810286908a4866aec448559e1d
adrianjacob <Adrian_Jacob@intuit.com>
2017-12-30 17:14:18 +0000

    testing adding new things

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,6 +3,7 @@ import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
+import MattressPreview from "./cms-preview-templates/mattress";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
@@ -23,6 +24,7 @@ class ColorControl extends React.Component {
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("mattress", MattressPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
